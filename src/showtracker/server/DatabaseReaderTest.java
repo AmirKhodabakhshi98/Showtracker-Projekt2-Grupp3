62352b07b1d9ec69ee65a25c6be5107dbd1a4217
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2021-02-17 16:25:48 +0100

    unit test test

--- src/showtracker/server/DatabaseReaderTest.java
+++ src/showtracker/server/DatabaseReaderTest.java
@@ -28,9 +28,38 @@ class DatabaseReaderTest {
 
     @org.junit.jupiter.api.Test
     void searchOMDBdbShows() {
+        String[][] array = new String[1][2];
+        array[0][0] = "Friends";
+        array[0][1] = "tt0108778";
         DatabaseReader databaseReader = new DatabaseReader();
-        databaseReader.searchOMDBdbShows("Friends");
+  //      databaseReader.searchOMDBdbShows("Friends");
+        assertArrayEquals (array,databaseReader.searchOMDBdbShows("Friends"));
+
     }
+    @org.junit.jupiter.api.Test
+    void searchOMDBdbShowsNull() {
+        String[][] array = new String[1][2];
+        array[0][0] = null;
+        array[0][1] = null;
+        DatabaseReader databaseReader = new DatabaseReader();
+        //      databaseReader.searchOMDBdbShows("Friends");
+//        assertArrayEquals (array,databaseReader.searchOMDBdbShows("Friends"));
+        assertArrayEquals(array,databaseReader.searchOMDBdbShows(""));
+    }
+
+    //Testar för serie med "totalseason: N/A"
+    @org.junit.jupiter.api.Test
+    void searchOMDBdbShowsTotalSeasonNA() {
+        String[][] array = new String[1][2];
+        array[0][0] = "Blacklist";
+        array[0][1] = "tt9892494";
+        DatabaseReader databaseReader = new DatabaseReader();
+        //      databaseReader.searchOMDBdbShows("Friends");
+//        assertArrayEquals (array,databaseReader.searchOMDBdbShows("Friends"));
+        assertArrayEquals(array,databaseReader.searchOMDBdbShows("Blacklist"));
+    }
+
+
 
     @org.junit.jupiter.api.Test
     void main() {
@@ -47,4 +76,10 @@ class DatabaseReaderTest {
     @org.junit.jupiter.api.Test
     void updateShow() {
     }
+
+    @org.junit.jupiter.api.Test
+    void searchTheTVDBShow2() {
+
+    }
+
 }
