ef2fcdeb09d6451f677a2f1d63b7c4ba73e505af
Edvin B <60152767+edvinbrus1@users.noreply.github.com>
2021-02-18 13:24:06 +0100

    Simple test cases for Envelope

--- src/showtracker/EnvelopeTest.java
+++ src/showtracker/EnvelopeTest.java
@@ -8,9 +8,26 @@ class EnvelopeTest {
 
     @Test
     void getType() {
+        Envelope envelope = new Envelope("Test", "Series");
+        assertEquals("Series", envelope.getType());
     }
 
     @Test
     void getContent() {
+        Envelope envelope = new Envelope("Test", "Series");
+        assertEquals("Test", envelope.getContent());
     }
+
+    @Test
+    void getTypeWhenNull(){
+        Envelope envelope = new Envelope(null, null);
+        assertEquals(null, envelope.getType());
+    }
+
+    @Test
+    void getContentWhenNull(){
+        Envelope envelope = new Envelope(null, null);
+        assertEquals(null, envelope.getContent());
+    }
+
 }
